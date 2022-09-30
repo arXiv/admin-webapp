dd1004d8abefcd08e269cfb578736f33e4d0050a
Brian D. Caruso <bdc34@cornell.edu>
2022-09-30 19:07:11 -0400

    Added ownership accepted and rejected

--- admin_webapp/routes/ownership.py
+++ admin_webapp/routes/ownership.py
@@ -1,5 +1,7 @@
 """arXiv paper ownership routes."""
 
+from datetime import datetime, timedelta
+
 from flask import Blueprint, render_template, url_for, request, \
     make_response, redirect, current_app, send_file, Response, flash
 
@@ -27,13 +29,54 @@ def pending() -> Response:
     per_page = args.get('per_page', default=12, type=int)
     page = args.get('page', default=1, type=int)
 
+    data = _ownership_listing('pending', per_page, page, 0)
+    data['title'] = "Ownership Reqeusts: Pending"
+    return render_template('ownership/requests.html',
+                           **data)
+
+
+@blueprint.route('/accepted', methods=['GET'])
+def accepted() -> Response:
+    """accepted ownership reqeusts."""
+    args = request.args
+    per_page = args.get('per_page', default=12, type=int)
+    page = args.get('page', default=1, type=int)
+    days_back = args.get('days_back', default=7, type=int)
+
+    data = _ownership_listing('accepted', per_page, page, days_back=days_back)
+    data['title'] = f"Ownership Reqeusts: accepted last {days_back} days"
+    return render_template('ownership/requests.html',
+                           **data)
+
+
+@blueprint.route('/rejected', methods=['GET'])
+def rejected() -> Response:
+    """Rejected ownership reqeusts."""
+    args = request.args
+    per_page = args.get('per_page', default=12, type=int)
+    page = args.get('page', default=1, type=int)
+    days_back = args.get('days_back', default=7, type=int)
+
+    data = _ownership_listing('rejected', per_page, page, days_back=days_back)
+    data['title'] = f"Ownership Reqeusts: Rejected last {days_back} days"
+    return render_template('ownership/requests.html',
+                           **data)
+
+
+
+def _ownership_listing(workflow_status:str, per_page:int, page: int,
+                       days_back:int) -> dict:
     session = get_state(current_app).db.session
 
     stmt = (select(OwnershipRequestsAudit)
             .options(selectinload(OwnershipRequestsAudit.user))
-            .where(OwnershipRequests.workflow_status == 'pending')
-            .limit(per_page).offset((page -1) * per_page)
-            )
+            .filter(OwnershipRequests.workflow_status == workflow_status))
+
+    if workflow_status in ('accepted', 'rejected'):
+        now = datetime.now() - timedelta(days=days_back)
+        stmt = stmt.filter(OwnershipRequestsAudit.date > now.timestamp())
+
+    stmt = stmt.limit(per_page).offset((page -1) * per_page)
     oreqs = session.scalars(stmt)
 
     stmt2 = (select(func.count(OwnershipRequests.request_id))
@@ -41,7 +84,4 @@ def pending() -> Response:
     count = session.execute(stmt2).scalar_one()
 
     pagination = Pagination(query=None, page=page, per_page=per_page, total=count, items=None)
-    return render_template('ownership/requests.html',
-                           title="Ownership Requests: Pending",
-                           ownership_requests=oreqs,
-                           pagination=pagination)
+    return dict(pagination=pagination, count=count, ownership_requests=oreqs, worflow_status=workflow_status, days_back=days_back)
